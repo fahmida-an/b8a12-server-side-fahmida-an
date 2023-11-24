e420c3a92b9d1578c0a094ae0fcef2caa8398b1b
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-11-25 01:12:39 +0600

    Added put method for save user in db

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const app =express();
 const cors = require('cors')
-require('dotenv').config
-const port = process.env.PORT || 4000;
+require('dotenv').config()
+const port = process.env.PORT || 5000;
 
 // middleware 
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdaizgq.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const newsCollection = client.db("NewsDb").collection("news");
+    const usersCollection = client.db("NewsDb").collection("users");
+
+    app.get("/news", async(req,res) => {
+        const result = await newsCollection.find().toArray()
+        res.send(result)
+    })
+    app.put('/users/:email', async (req, res) => {
+        const email = req.params.email
+        const user = req.body
+        const query = { email: email }
+        const options = { upsert: true }
+        const isExist = await usersCollection.findOne(query)
+        console.log('User found?----->', isExist)
+        if (isExist) return res.send(isExist)
+        const result = await usersCollection.updateOne(
+          query,
+          {
+            $set: { ...user, timestamp: Date.now() },
+          },
+          options
+        )
+        res.send(result)
+      })
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
