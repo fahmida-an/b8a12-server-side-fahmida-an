5a0e36c201a8ec8f1a10ba6a8ce11b10046551cf
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-11-26 00:41:10 +0600

    Added payment system

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express')
 const app =express();
 const cors = require('cors')
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware 
 
 app.use(cors())
+app.use(express.static("public"))
 app.use(express.json())
 
 
@@ -30,18 +32,55 @@ async function run() {
     // await client.connect();
     const newsCollection = client.db("NewsDb").collection("news");
     const usersCollection = client.db("NewsDb").collection("users");
+    const premiumCollection = client.db("NewsDb").collection("premium");
+    const premiumPackageCollection = client.db("NewsDb").collection("premiumPackage");
+    const paymentCollection = client.db("NewsDb").collection("payments");
 
     app.get("/news", async(req,res) => {
         const result = await newsCollection.find().toArray()
         res.send(result)
     })
+
+
+
+    app.post("/premiumPackage", async(req,res)=> {
+      const premiumUser = req.body;
+      const result = await premiumPackageCollection.insertOne(premiumUser)
+      res.send(result)
+    })
+
+
+    // app.get("/premiumPackage", async(req,res) => {
+    //   const email = req.query.email;
+    //   const query = {email: email}
+    //   const result = premiumPackageCollection.find(query).toArray()
+    //   res.send(result)
+    // })
+
+    app.get("/premiumPackage", async (req, res) => {
+      try {
+        const email = req.query.email;
+        const query = { email: email };
+        const result = await premiumPackageCollection.find(query).toArray();  
+        res.send(result);
+      } catch (error) {
+        console.error("Error retrieving premium package information:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    app.get("/premium", async(req,res) => {
+      const result = await premiumCollection.find().toArray()
+      res.send(result)
+    })
+
     app.put('/users/:email', async (req, res) => {
         const email = req.params.email
         const user = req.body
         const query = { email: email }
         const options = { upsert: true }
         const isExist = await usersCollection.findOne(query)
-        console.log('User found?----->', isExist)
+        console.log('User', isExist)
         if (isExist) return res.send(isExist)
         const result = await usersCollection.updateOne(
           query,
@@ -52,6 +91,47 @@ async function run() {
         )
         res.send(result)
       })
+
+      //payment intent
+      // 1.
+      app.post('/create-payment-intent', async(req,res) => {
+        const {price} = req.body;
+        const amount = parseInt(price*100)
+        console.log(amount, 'amount inside the intent');
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount:amount,
+          currency: 'usd',
+          payment_method_types: ['card']
+
+        })
+        res.send({
+          clientSecret: paymentIntent.client_secret
+        })
+      })
+
+      // 2 payment api
+
+      app.post('/payments', async(req,res) => {
+        const payment = req.body;
+        const paymentResult = await paymentCollection.insertOne(payment)
+        const userEmail = payment.email;
+
+        const updateResult = await usersCollection.updateOne(
+          { email: userEmail, role: 'normal' }, // Find the user by email and role 'normal'
+          { $set: { role: 'premium' } } // Update the role to 'premium'
+        );
+
+        if (updateResult.modifiedCount === 1) {
+          res.status(200).send('Payment successful, user role updated to premium.');
+        } else {
+          res.status(500).send('Payment successful, but failed to update user role to premium.');
+
+          
+        }
+        res.send(paymentResult, updateResult)
+      })
+
+
 
 
 
