e6606ad9fa83ccb493b50e6c79e5fad26ce6543e
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-11-28 02:06:41 +0600

    generate jwt token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const app =express();
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
@@ -36,6 +37,15 @@ async function run() {
     const premiumPackageCollection = client.db("NewsDb").collection("premiumPackage");
     const paymentCollection = client.db("NewsDb").collection("payments");
 
+    // jwt api 
+
+    app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '365d'})
+      res.send({token})
+    })
+
+    //news api
     app.get("/news", async(req,res) => {
         const result = await newsCollection.find().toArray()
         res.send(result)
@@ -54,6 +64,31 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/news/:id', async(req,res)=> {
+      const id = req.params.id;
+      const query ={ _id: new ObjectId(id)}
+      const result = await newsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.patch('/news/approve/:id', async (req, res) => {
+      const newsId = req.params.id;
+    
+      try {
+        const filter = { _id: new ObjectId(newsId) };
+        const updatedDoc = {
+          $set: {
+            status: 'approved'
+          }
+        };
+        const result = await newsCollection.updateOne(filter, updatedDoc);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
+   
+
 
     app.post("/premiumPackage", async(req,res)=> {
       const premiumUser = req.body;
@@ -61,13 +96,6 @@ async function run() {
       res.send(result)
     })
 
-
-    // app.get("/premiumPackage", async(req,res) => {
-    //   const email = req.query.email;
-    //   const query = {email: email}
-    //   const result = premiumPackageCollection.find(query).toArray()
-    //   res.send(result)
-    // })
 
     app.get("/premiumPackage", async (req, res) => {
       try {
@@ -86,7 +114,7 @@ async function run() {
       res.send(result)
     })
 
-    // user api #
+    // user api 
 
     app.get('/users',async(req,res) => {
       const result = await usersCollection.find().toArray();
